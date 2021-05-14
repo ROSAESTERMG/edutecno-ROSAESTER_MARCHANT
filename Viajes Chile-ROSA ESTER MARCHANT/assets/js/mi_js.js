c4cea45037913b40052c5b6fb549e4573d06a09b
ROSA ESTER M <rosita.marchant@gmail.com>
2021-05-14 00:36:55 -0400

    Update mi_js.js

--- Viajes Chile-ROSA ESTER MARCHANT/assets/js/mi_js.js
+++ Viajes Chile-ROSA ESTER MARCHANT/assets/js/mi_js.js
@@ -1,7 +1,4 @@
-$('#modal1').on('hidden.bs.modal', function (e) {
-  // do something...
-  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
-});
+
 // JavaScript Document
 
    $(window).scroll(function() {
@@ -13,79 +10,19 @@ $('#modal1').on('hidden.bs.modal', function (e) {
     });
 
 
-$(document).ready(function() {
 
-    var scrollLink = $('.scroll');
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  var scrollLink = $('.scroll');
     scrollLink.click(function(e) {
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top
         }, 1500);
     });
-
-    
-});
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
- 
 })
-
-
-
-/*$('.image-link').magnificPopup({type:'image'});*/
-
-/*------------autopaly videos al pasar el mosue---------*/
-/*---- video 1----*/
-$("#video1").mouseenter(function(){
-  
-  $(this).attr("src",$(this).attr("src") + "?autoplay=1");
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
 });
-
-$("#video1").mouseleave(function(){
-  var src= $(this).attr("src");
-  var arr_str = src.split("?");
-  $(this).attr("src",arr_str[0]);
-});
-
-
-/*---- video 2----*/
-$("#video2").mouseenter(function(){
-  
-  $(this).attr("src",$(this).attr("src") + "?autoplay=1");
-});
-
-$("#video2").mouseleave(function(){
-  var src= $(this).attr("src");
-  var arr_str = src.split("?");
-  $(this).attr("src",arr_str[0]);
-});
-
-
-/*---- video 3----*/
-$("#video3").mouseenter(function(){
-  
-  $(this).attr("src",$(this).attr("src") + "?autoplay=1");
-});
-
-$("#video3").mouseleave(function(){
-  var src= $(this).attr("src");
-  var arr_str = src.split("?");
-  $(this).attr("src",arr_str[0]);
-});
-
-/*---- video 4----*/
-$("#video4").mouseenter(function(){
-  
-  $(this).attr("src",$(this).attr("src") + "?autoplay=1");
-});
-
-$("#video4").mouseleave(function(){
-  var src= $(this).attr("src");
-  var arr_str = src.split("?");
-  $(this).attr("src",arr_str[0]);
-});
-
-/*--------fin----autopaly videos al pasar el mosue---------*/
-
-
