79d9f81efad49c220567784ee7030dc9ddd49cd6
ROSA ESTER M <rosita.marchant@gmail.com>
2021-05-16 01:42:38 -0400

    TRABAJO FINA VIAJES CHILE

--- Viajes Chile-ROSA ESTER MARCHANT/assets/js/mi_js.js
+++ Viajes Chile-ROSA ESTER MARCHANT/assets/js/mi_js.js
@@ -1,46 +1,52 @@
 // JavaScript Document
 
-   $(window).scroll(function() {
-        if ($("#menu").offset().Top > 100) {
-            $("#menu").addClass("menu_transp");
-        } else {
-            $("#menu").removeClass("menu_transp");
-        }
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+      $("nav").addClass("scrolled");
+    } else {
+      $("nav").removeClass("scrolled");
+    }
+  });
+
+
+  $('[data-toggle="tooltip"]').tooltip()
+  //---- funcion que destaca sección seleccionada
+  $('li').click(function () {
+    $('li').each(function () {
+      //Recorremos todos los "li" para quitarles los fondos
+      $(this).css('background-color', '');
     });
+    //Se lo añadimos al que se le hace el click
+    $(this).css('background-color', '#387B8F');
+  });
 
 
-$(document).ready(function() {
-
-    var scrollLink = $('.scroll');
-
-    scrollLink.click(function(e) {
-        e.preventDefault();
-        $('body,html').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 1500);
-    });
+});
 
 
-    $(window).scroll(function() {
+//-----     vista modal de los videos -----------	
+$('#modal1').on('hidden.bs.modal', function () {
 
-        var scrollbarLocation = $(this).scrollTop();
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
 
-        scrollLink.each(function() {
-            var sectionOffset = $(this.hash).offset().top - 20;
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('transp');
-                $(this).parent().siblings().removeClass('transp');
-            }
-        });
-
-
-        $("#navbar").toggleClass('scrolled', $(this).scrollTop() > $('#link1').height());
-    });
-
-
-    $('[data-toggle="tooltip"]').tooltip()
+$('#modal12').on('hidden.bs.modal', function () {
+  // do something...
+  $('#modal12 iframe').attr("src", $("#modal12 iframe").attr("src"));
+});
+$('#modal13').on('hidden.bs.modal', function () {
+  // do something...
+  $('#modal13 iframe').attr("src", $("#modal13 iframe").attr("src"));
+});
+$('#modal14').on('hidden.bs.modal', function () {
+  $('#modal14 iframe').attr("src", $("#modal14 iframe").attr("src"));
+});
 
 
-
+//---------- evento onload para así mostrar mensaje de carga pagina inicial ---//
+$(window).on("load", function () {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
 });
