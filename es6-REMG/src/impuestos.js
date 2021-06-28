bbf06b430a91d39edb90ba4b5604785f98722a4b
ROSA ESTER M <rosita.marchant@gmail.com>
2021-06-27 20:47:44 -0400

    OBJETOS 2

--- es6-REMG/src/impuestos.js
+++ es6-REMG/src/impuestos.js
@@ -8,21 +8,7 @@ constructor(monto,deducible){
 this.monto = monto;
 this.deducible = deducible;
 }
-/*    
-get monto(){
-return this._monto;
-}
-set monto(nuevo_monto){
-this._monto = nuevo_monto;
-}
-    
-get deducible(){
-return this._deducible;
-}
-set deducible(nuevo_deducible){
-this._deducible = nuevo_deducible;
-}   
- */   
+ 
     
 calcular(){
 return (((this.monto * this.deducible)*21)/100);
@@ -30,8 +16,6 @@ return (((this.monto * this.deducible)*21)/100);
     
 }
 
-
-// (monto_bruto_anual − deducciones) * 21%)
 
 
     
--- es6-REMG/src/index.js
+++ es6-REMG/src/index.js
@@ -3,21 +3,7 @@
 import Persona from './persona.js';
 import Impuestos from './impuestos.js';
 
-var $texto = '';
 
-//----------------fin ----paciente----------------------------//
-/*
-Consultorio.prototype.setAgregarPaciente = function (paciente_nuevo) {
-  this.paciente.push(paciente_nuevo);
-}
-
-var paciente1 = new Paciente('Marcelo', 20, '111-1', 'ANEMIA');
-var consultorio1 = new Consultorio('Arica', [paciente1]);
-*/
-
-
-
-//----------- guardar pacientes ------------------//
 let guardar = document.getElementById('guardar');
 guardar.addEventListener('click',guardarpersona);
 
@@ -29,7 +15,8 @@ event.preventDefault();
     
  if (m1 && d1 > 0) {
  let resultado = new Impuestos(m1,d1);
-div_mostrar.innerHTML =resultado.calcular();
+ let resultado = new Impuestos(m1,d1);
+div_mostrar.innerHTML =' Sr(a) : <br>Monto Bruto:'+resultado.monto+'  <br>Deducible:'+resultado.deducible+' <br>*** TOTAL IMPUESTO:'+resultado.calcular()+' ***';
 }
 else {
 div_mostrar.innerHTML = 'Debe ingresar un número... ';
