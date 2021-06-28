2a1c6259550673fc4778dbdb46af7716afa18b8d
ROSA ESTER M <rosita.marchant@gmail.com>
2021-06-27 20:16:18 -0400

    objetos 2

--- es6-REMG/dist/index.js
+++ es6-REMG/dist/index.js
@@ -19,36 +19,26 @@ var consultorio1 = new Consultorio('Arica', [paciente1]);
 */
 //----------- guardar pacientes ------------------//
 
-var guardar = function guardar() {
+var guardar = document.getElementById('guardar');
+guardar.addEventListener('click', guardarpersona);
+
+function guardarpersona() {
+  event.preventDefault();
   var n1 = document.getElementById('nombre').value;
   var m1 = parseInt(document.getElementById('monto').value);
   var d1 = parseInt(document.getElementById('deducible').value);
-  var impuestox = new _impuestos["default"](m1, d1);
-  console.log(impuestox.monto);
-  console.log(impuestox.deducible);
-  /*
-  
-   var impuestox = new Pers(m1,d1);
-   persona1.setAgregarPersona(impuestox);
-  */
 
+  if (m1 && d1 > 0) {
+    var resultado = new _impuestos["default"](m1, d1);
+    div_mostrar.innerHTML = _impuestos["default"].calcular();
+  } else {
+    div_mostrar.innerHTML = 'Debe ingresar un número... ';
+    div_mostrar.innerHTML = ' y este debe ser mayor que cero';
+  }
+
+  ;
   document.getElementById("nombre").value = '';
   document.getElementById("monto").value = '';
   document.getElementById("deducible").value = '';
   alert('datos ingresados');
-}; //----------- mostrar pacientes ------------------//
-
-
-var mostrar = function mostrar() {
-  var divr = document.getElementById("div_mostrar");
-  var $i = 0;
-  $texto = '<br><br><p ">-----------------  LISTADO DE PACIENTES -------------- </p>';
-
-  for ($i = 0; $i < consultorio1.paciente.length; $i++) {
-    $texto += '<p>NOMBRE:' + consultorio1.paciente[$i].getNombre() + ', EDAD:' + consultorio1.paciente[$i].getEdad() + ', RUT:';
-    $texto += consultorio1.paciente[$i].getRut() + ', DIAGNOSTICO:' + consultorio1.paciente[$i].getDiag() + '</p>';
-    console.log(consultorio1.paciente[$i].nombre);
-  }
-
-  document.getElementById("div_mostrar").innerHTML = $texto;
-};
+}
--- es6-REMG/src/index.js
+++ es6-REMG/src/index.js
@@ -23,55 +23,24 @@ guardar.addEventListener('click',guardarpersona);
 
 function guardarpersona() {
 event.preventDefault();
-  let n1 = document.getElementById('nombre').value;
-  let m1 = parseInt(document.getElementById('monto').value);
-  let d1 = parseInt(document.getElementById('deducible').value);
+  let n1 = document.getElementById('nombre_p').value;
+  let m1 = parseInt(document.getElementById('monto_p').value);
+  let d1 = parseInt(document.getElementById('deducible_p').value);
     
  if (m1 && d1 > 0) {
- 
-let resultado = new Impuestos(m1,d1);
+ let resultado = new Impuestos(m1,d1);
 div_mostrar.innerHTML = Impuestos.calcular();
-
-}else {
-div_mostrar.innerHTML = `Debe ingresar un número... `;
-div_mostrar.innerHTML = ` y este debe ser mayor que cero`;
+}
+else {
+div_mostrar.innerHTML = 'Debe ingresar un número... ';
+div_mostrar.innerHTML = ' y este debe ser mayor que cero';
 };
-
-    
-    
-    
-    
-
    
     
- /*
-
-  var impuestox = new Pers(m1,d1);
-  persona1.setAgregarPersona(impuestox);
- */
-    
-  document.getElementById("nombre").value = '';
-  document.getElementById("monto").value = '';
-  document.getElementById("deducible").value = '';
+  document.getElementById("nombre_p").value = '';
+  document.getElementById("monto_p").value = '';
+  document.getElementById("deducible_p").value = '';
   alert('datos ingresados');
 }
 
-
-//----------- mostrar pacientes ------------------//
-const mostrar= () => {
-  var divr = document.getElementById("div_mostrar")
-  var $i = 0;
-
-
-  $texto = '<br><br><p ">-----------------  LISTADO DE PACIENTES -------------- </p>';
-
-
-      for ($i = 0; $i < consultorio1.paciente.length; $i++) {
-      $texto += '<p>NOMBRE:' + consultorio1.paciente[$i].getNombre() + ', EDAD:' + consultorio1.paciente[$i].getEdad() + ', RUT:';   
-         $texto += consultorio1.paciente[$i].getRut() + ', DIAGNOSTICO:' + consultorio1.paciente[$i].getDiag() + '</p>';
-   console.log(consultorio1.paciente[$i].nombre);
-  }
-
-  document.getElementById("div_mostrar").innerHTML = $texto;
-}
 
