377b85b1e12445ba29ebfb9520fc73798ecf4945
ROSA ESTER M <rosita.marchant@gmail.com>
2021-06-29 21:30:55 -0400

    Update index_herencias.js

--- HERENCIAS-REMG/src/index_herencias.js
+++ HERENCIAS-REMG/src/index_herencias.js
@@ -40,71 +40,6 @@ console.log(`El nombre del dueño es: ${this.nombre}. El domicilio es: ${this.di
 
 
 
-
-//------------------------------------------
-let guardar = document.getElementById('guardar');
-guardar.addEventListener('click',guardarpersona);
-
-
-//------------------------------------------
-function guardarpersona() {
-	    let valor = document.getElementById("TIPO").value;
-       // var valor = valor.value;
-
-    let n1 = document.getElementById('nom_due').value;
-    let n2 = document.getElementById('tele').value;
-    let n3 = document.getElementById('domi').value;
-    let n4 = document.getElementById('nom_masc').value;
-    let n6 = document.getElementById('motivo').value;
-    
-    if (valor=='GATO'){
-
-    const gato = new Gatos(valor,n4, n6);
-     console.log(gato.tipo);
-        texto='<p align="center">DATOS GUARDADOS';
-        texto+='<br>';
-         texto+='<br><li align="center">'+gato.info()+'</li></p>';
-        //console.log(gato.info());
-
- }
-else
-   if (valor=='PERRO'){
-const perro = new Perros(valor,n4, n6);
-       console.log(perro.tipo);
-       texto='<p align="center">DATOS GUARDADOS';
-        texto+='<br>';
-         texto+='<br><li align="center">'+perro.info()+'</li></p>';
-       //console.log(perro.info());
- }
-else
-    if (valor=='CONEJO'){
-const conejo = new Conejos(valor,n4, n6);
-        console.log(conejo.tipo);
-        texto='<p align="center">DATOS GUARDADOS';
-        texto+='<br>';
-         texto+='<br><li align="center">'+conejo.info()+'</li></p>';
-       // console.log(conejo.info());
- }
-
-
-    
-
-  
-div_mostrar.innerHTML  = texto;
-
-document.getElementById('nom_due').value = '';
-document.getElementById('tele').value = '';
-document.getElementById('domi').value = '';
-document.getElementById('nom_masc').value = '';
-document.getElementById('TIPO').value = 'PERRO';
-document.getElementById('motivo').value = '';
-}
-
-
-
-
-
-
 //--------------Animal------
 class Animal extends Dueno {
 constructor( tipo) {
@@ -208,6 +143,67 @@ info() {
 return `El tipo de animal es un:${this.tipo} , mientras que el nombre de la mascota es: ${this.nombre_mas} y la enfermedad es:${this.motivo}`;
 }
 
+}
+
+
+
+//------------------------------------------
+let guardar = document.getElementById('guardar');
+guardar.addEventListener('click',guardarpersona);
+
+
+//------------------------------------------
+function guardarpersona() {
+	    let valor = document.getElementById("TIPO").value;
+       // var valor = valor.value;
+
+    let n1 = document.getElementById('nom_due').value;
+    let n2 = document.getElementById('tele').value;
+    let n3 = document.getElementById('domi').value;
+    let n4 = document.getElementById('nom_masc').value;
+    let n6 = document.getElementById('motivo').value;
+    
+    if (valor=='GATO'){
+
+    const gato = new Gatos(valor,n4, n6);
+     console.log(gato.tipo);
+        texto='<p align="center">DATOS GUARDADOS';
+        texto+='<br>';
+         texto+='<br><li align="center">'+gato.info()+'</li></p>';
+        //console.log(gato.info());
+
+ }
+else
+   if (valor=='PERRO'){
+const perro = new Perros(valor,n4, n6);
+       console.log(perro.tipo);
+       texto='<p align="center">DATOS GUARDADOS';
+        texto+='<br>';
+         texto+='<br><li align="center">'+perro.info()+'</li></p>';
+       //console.log(perro.info());
+ }
+else
+    if (valor=='CONEJO'){
+const conejo = new Conejos(valor,n4, n6);
+        console.log(conejo.tipo);
+        texto='<p align="center">DATOS GUARDADOS';
+        texto+='<br>';
+         texto+='<br><li align="center">'+conejo.info()+'</li></p>';
+       // console.log(conejo.info());
+ }
+
+
+    
+
+  
+div_mostrar.innerHTML  = texto;
+
+document.getElementById('nom_due').value = '';
+document.getElementById('tele').value = '';
+document.getElementById('domi').value = '';
+document.getElementById('nom_masc').value = '';
+document.getElementById('TIPO').value = 'PERRO';
+document.getElementById('motivo').value = '';
 }
 
 
