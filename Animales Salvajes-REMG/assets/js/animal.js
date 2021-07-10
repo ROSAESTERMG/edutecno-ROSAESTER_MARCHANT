0285aa6ae9bee837af8361fd96e704368a33c1df
ROSA ESTER M <rosita.marchant@gmail.com>
2021-07-09 23:39:51 -0400

    PRUEBA ANIMALES

--- Animales Salvajes-REMG/assets/js/animal.js
+++ Animales Salvajes-REMG/assets/js/animal.js
@@ -3,7 +3,7 @@
 //------------------CLASE ANIMAL -----------------//
 
 export default class Animalito{
-constructor(nombre,edad,img,obs){
+constructor(nombre,edad,obs,img){
 this._nombre = nombre;
 this._edad = edad;
 this._img = img;
@@ -38,10 +38,7 @@ return this._obs;
 set obs(nuevo_obs){
 this._obs = nuevo_obs;
 }   
-     
-/*datosanimal() {
-    return (`El nombre del dueño es: ${this.nombre}. El domicilio es: ${this.direccion} y el número de teléfono es:${this.telefono}`);
-  }*/
+
     mostrardatos(tipo_animal){
 
     alert('llegó el animal'+tipo_animal);
@@ -63,47 +60,4 @@ class Animalsonido extends Animal {
   }
 }
 */
-//---------------leon---------------------
-class Leon extends Animalito {
-  constructor(nombre,edad,img,obs, sonido) {
-   super(nombre,edad,img,obs)
-    this._sonido = sonido;
-  }
 
-  get sonido() {
-    return this._sonido
-  }
-  set sonido(nuevo_sonido) {
-    this._sonido = nuevo_sonido;
-  }
-}
-
-//---------------lobo---------------------
-class Lobo extends Animalito {
-  constructor(nombre,edad,img,obs, sonido) {
-    super(nombre,edad,img,obs, sonido)
- }
-//return('Aullido.mp3')
-}  
-
-//---------------oso---------------------
-class Oso extends Animalito {
-  constructor(nombre,edad,img,obs, sonido) {
-    super(nombre,edad,img,obs, sonido)
- }
-//return('Grunido.mp3')
-}  
-//---------------serpiente---------------------
-class Serpiente extends Animalito {
-  constructor(nombre,edad,img,obs, sonido) {
-    super(nombre,edad,img,obs, sonido)
- }
-//return('Siseo.mp3')
-}  
-//---------------aguila---------------------
-class Aguila extends Animalito {
-  constructor(nombre,edad,img,obs, sonido) {
-    super(nombre,edad,img,obs, sonido)
- }
-//return('Chillido.mp3')
-}  
--- Animales Salvajes-REMG/assets/js/index.js
+++ Animales Salvajes-REMG/assets/js/index.js
@@ -1,8 +1,81 @@
 // JavaScript Document
-import Clinica from './clinica.js';
 import Animalito from './animal.js';
-
 var $texto = '';
+
+//---------------------clases de animales ----------------------
+//---------------leon---------------------
+class Leon extends Animalito {
+  constructor(nombre,edad,obs,img, sonido) {
+   super(nombre,edad,img,obs)
+    this._sonido = sonido;
+  }
+
+  get sonido() {
+    return this._sonido
+  }
+  set sonido(nuevo_sonido) {
+    this._sonido = nuevo_sonido;
+  }
+}
+
+//---------------lobo---------------------
+class Lobo extends Animalito {
+  constructor(nombre,edad,obs,img, sonido) {
+    super(nombre,edad,img,obs, sonido)
+ }
+  get sonido() {
+    return this._sonido
+  }
+  set sonido(nuevo_sonido) {
+    this._sonido = nuevo_sonido;
+  }
+}  
+
+//---------------oso---------------------
+class Oso extends Animalito {
+  constructor(nombre,edad,obs,img, sonido) {
+    super(nombre,edad,img,obs, sonido)
+ }
+
+  get sonido() {
+    return this._sonido
+  }
+  set sonido(nuevo_sonido) {
+    this._sonido = nuevo_sonido;
+  }
+}  
+//---------------serpiente---------------------
+class Serpiente extends Animalito {
+  constructor(nombre,edad,obs,img, sonido) {
+    super(nombre,edad,img,obs, sonido)
+ }
+
+  get sonido() {
+    return this._sonido
+  }
+  set sonido(nuevo_sonido) {
+    this._sonido = nuevo_sonido;
+  }
+}  
+//---------------aguila---------------------
+class Aguila extends Animalito {
+  constructor(nombre,edad,obs,img, sonido) {
+    super(nombre,edad,img,obs, sonido)
+ }
+
+  get sonido() {
+    return this._sonido
+  }
+  set sonido(nuevo_sonido) {
+    this._sonido = nuevo_sonido;
+  }
+}  
+
+
+
+
+
+
 
 
 
@@ -15,34 +88,34 @@ var e0 = document.getElementById('edad').selectedIndex;
 var e1 = document.getElementById('edad').value;
 var c1 = document.getElementById('comentarios').value;
 var i1 = document.getElementById('preview').style.backgroundImage;
-
 if (a0>0 && e0>0 && c1!='' ) {
 if (a0==1){
-//let animalx = new Leon(a1,e1,c1,i1);
-   var animalx = new Leon(a1,e1,c1,i1);
-
-console.log(animalx.nombre);
-    
+    let animalx = new Leon(a1,e1,c1,i1,'Rugido.mp3');
     $texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Leon.mostrardatos("'+a1+'")"></div>  </div>';
+console.log(animalx);
 }
 else
 if (a0==2){
-    let animalx = new Lobo(a1,e1,c1,i1);
+    let animalx = new Lobo(a1,e1,c1,i1,'Aullido.mp3');
+    $texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Lobo.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==3){
-    let animalx = new Oso(a1,e1,c1,i1); 
+    let animalx = new Oso(a1,e1,c1,i1,'Grunido.mp3'); 
+    $texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Oso.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==4){
-    let animalx = new Serpiente(a1,e1,c1,i1);
+    let animalx = new Serpiente(a1,e1,c1,i1,'Siseo.mp3');
+    $texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Serpiente.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==5){
-     let animalx = new Aguila(a1,e1,c1,i1);
+     let animalx = new Aguila(a1,e1,c1,i1,'Chillido.mp3');
+    $texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Aguila.mostrardatos("'+a1+'")"></div>  </div>';
 }
-    let clinicax = new Clinica('Australia', [animalx]); 
-$texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="animalx.mostrardatos("'+a1+'")"></div>  </div>';
+
+//$texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="animalx.mostrardatos("'+a1+'")"></div>  </div>';
 
 document.getElementById('Animales').innerHTML =$texto;
 alert('datos ingresados');
@@ -53,7 +126,7 @@ else
 
 
 
-//------------------------------------------
+//------------guardar animalito------------------------------
 let guardar = document.getElementById('btnRegistrar');
 guardar.addEventListener('click', guardar_animal);
 
@@ -85,21 +158,4 @@ nombre_ima='Aguila.png';
 return (nombre_ima)
 }
 
-//----------- mostrar animal ------------------//
-const mostrar_animal = () => {
-  var divr = document.getElementById("div_mostrar")
-  var $i = 0;
-
-
-  $texto = '<br><br><p ">-----------------  animales -------------- </p>';
-
-
-      for ($i = 0; $i < consultorio1.paciente.length; $i++) {
-      $texto += '<p>NOMBRE:' + consultorio1.paciente[$i].getNombre() + ', EDAD:' + consultorio1.paciente[$i].getEdad() + ', RUT:';   
-         $texto += consultorio1.paciente[$i].getRut() + ', DIAGNOSTICO:' + consultorio1.paciente[$i].getDiag() + '</p>';
-   console.log(consultorio1.paciente[$i].nombre);
-  }
-
-  document.getElementById("div_mostrar").innerHTML = $texto;
-}
 
