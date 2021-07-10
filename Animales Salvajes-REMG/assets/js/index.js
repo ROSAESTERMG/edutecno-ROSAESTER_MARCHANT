e6f24742f3f52a251c93de976d30a48063d9412c
ROSA ESTER M <rosita.marchant@gmail.com>
2021-07-10 00:29:04 -0400

    PRUEBA ANIMALES

--- Animales Salvajes-REMG/assets/js/index.js
+++ Animales Salvajes-REMG/assets/js/index.js
@@ -2,6 +2,8 @@
 import Animalito from './animal.js';
 var $texto = '';
 
+
+
 //---------------------clases de animales ----------------------
 //---------------leon---------------------
 class Leon extends Animalito {
@@ -92,30 +94,28 @@ var i1 = document.getElementById('preview').style.backgroundImage;
 if (a0>0 && e0>0 && c1!='' ) {
 
 if (a0==1){
-    
     let animalx = new Leon(a1,e1,c1,i1,'Rugido.mp3');
-    $texto = '<div id="leon" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Leon.mostrardatos("'+a1+'")"></div>  </div>';
-console.log(animalx);
+    $texto += '<div id="leon" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Leon.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==2){
     let animalx = new Lobo(a1,e1,c1,i1,'Aullido.mp3');
-    $texto = '<div id="lobo" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Lobo.mostrardatos("'+a1+'")"></div>  </div>';
+    $texto += '<div id="lobo" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Lobo.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==3){
     let animalx = new Oso(a1,e1,c1,i1,'Grunido.mp3'); 
-    $texto = '<div id="oso" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Oso.mostrardatos("'+a1+'")"></div>  </div>';
+    $texto += '<div id="oso" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Oso.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==4){
     let animalx = new Serpiente(a1,e1,c1,i1,'Siseo.mp3');
-    $texto = '<div id="serpiente" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Serpiente.mostrardatos("'+a1+'")"></div>  </div>';
+    $texto += '<div id="serpiente" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Serpiente.mostrardatos("'+a1+'")"></div>  </div>';
 }
 else
 if (a0==5){
      let animalx = new Aguila(a1,e1,c1,i1,'Chillido.mp3');
-    $texto = '<div id="Aguila" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Aguila.mostrardatos("'+a1+'")"></div>  </div>';
+    $texto += '<div id="Aguila" class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="Aguila.mostrardatos("'+a1+'")"></div>  </div>';
 }
 
 //$texto = '<div class="card" style="width: 300px;background-color: #848080;"><img class="card-img-top" src="assets/imgs/'+cambiar_animal(a1)+'"  alt="Foto Animal Registrado">    <div class="card-body"><img src="assets/imgs/audio.svg"  height="30px" alt=""/ onclic="animalx.mostrardatos("'+a1+'")"></div>  </div>';
